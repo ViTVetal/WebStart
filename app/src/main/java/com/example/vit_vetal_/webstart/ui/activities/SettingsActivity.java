2e75b41682a7fff73c1bc35f38cd3cc42ad1300f
ViTVetal <vvk1992@ukr.net>
2016-10-17 23:04:18 +0300

    fix

--- app/src/main/java/com/example/vit_vetal_/webstart/ui/activities/SettingsActivity.java
+++ app/src/main/java/com/example/vit_vetal_/webstart/ui/activities/SettingsActivity.java
@@ -58,7 +58,8 @@ public class SettingsActivity extends Activity {
         } else {
             String deviceId = getDeviceId(this);
 
-            if(deviceId == null || TextUtils.isEmpty(deviceId) || Long.parseLong(deviceId) <= 0) {
+            if(deviceId == null || TextUtils.isEmpty(deviceId) || deviceId.equals("unknown")
+                    || deviceId.equals("000000000000000")) {
                 SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
                 if(TextUtils.isEmpty(sharedPref.getString(EMULATOR_ID_KEY, ""))) {
                     deviceId = String.valueOf(Calendar.getInstance().getTimeInMillis());
@@ -170,7 +171,8 @@ public class SettingsActivity extends Activity {
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     String deviceId = getDeviceId(this);
 
-                    if(deviceId == null || TextUtils.isEmpty(deviceId) || Long.parseLong(deviceId) <= 0) {
+                    if(deviceId == null || TextUtils.isEmpty(deviceId) || deviceId.equals("unknown")
+                            || deviceId.equals("000000000000000")) {
                         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
                         if(TextUtils.isEmpty(sharedPref.getString(EMULATOR_ID_KEY, ""))) {
                             deviceId = String.valueOf(Calendar.getInstance().getTimeInMillis());
